990e133a0f2b18eb9d1d98715a6630ceb473561e
7konigsberg <77811205+7konigsberg@users.noreply.github.com>
2024-03-26 20:51:46 -0400

    practicum 11

---
+++ src/main/java/edu/iu/zzahid/primesservice/service/IAuthenticationService.java
@@ -0,0 +1,10 @@
+package edu.iu.zzahid.primesservice.service;
+
+import edu.iu.zzahid.primesservice.model.Customer;
+
+import java.io.IOException;
+
+public interface IAuthenticationService {
+    boolean register(Customer customer) throws IOException;
+//    boolean login(String username, String password) throws IOException;
+}
